93ca89e942621c12617f2bb8c30b4623285056d6
krishna <swrp.vicky@gmail.com>
2019-08-25 02:01:08 -0400

    added regimen changes

--- src/app/global.constants.ts
+++ src/app/global.constants.ts
@@ -4,4 +4,4 @@ export const PATIENT_TYPES_ENDPOINT = API_ENDPOINT + '/patientTypes';
 export const CANCER_TYPES_ENDPOINT = API_ENDPOINT + '/cancerTypes' + '/';
 export const SUB_CANCERTYPES_ENDPOINT = API_ENDPOINT + '/subCancerTypes/';
 export const SUB_CANCERTYPES2_ENDPOINT = API_ENDPOINT + '/subCancerTypes2/';
-export const API_URL = 'http://35.194.2.89:8080/';
+export const API_URL = 'https://104.197.116.178:8080/';

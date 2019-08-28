b38fa7daf1df2c26798b77c1dcd12ba47a8aa8dc
krishna <swrp.vicky@gmail.com>
2019-08-28 14:36:20 -0400

    done

--- src/app/global.constants.ts
+++ src/app/global.constants.ts
@@ -1,7 +1,7 @@
 
-export const API_ENDPOINT = `${window.location.origin}`;
+export const API_ENDPOINT = `${window.location.origin}/`;
 export const PATIENT_TYPES_ENDPOINT = API_ENDPOINT + '/patientTypes';
 export const CANCER_TYPES_ENDPOINT = API_ENDPOINT + '/cancerTypes' + '/';
 export const SUB_CANCERTYPES_ENDPOINT = API_ENDPOINT + '/subCancerTypes/';
 export const SUB_CANCERTYPES2_ENDPOINT = API_ENDPOINT + '/subCancerTypes2/';
-export const API_URL = 'https://104.197.116.178:8080/';
+export const API_URL = API_ENDPOINT;
